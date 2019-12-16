81c6e6763076d23e8eb7c73b07992b414f924710
tobilastik <rajioluwatobih@gmail.com>
2019-12-16 09:47:36 +0100

    Updated single room page

--- src/screen/SingleRoom.js
+++ src/screen/SingleRoom.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Background from '../components/Background';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../Context';
 
 export default class SingleRoom extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {};
+  }
+
+  componentDidMount () {}
+
   render () {
     return (
       <div>
-        Hello from Single room
+        This is Single room page
       </div>
     );
   }
